4c4fa2599161a8c194bbd2a0eab90526f2a9231f
Ibrahim shamma <i.abushammah@gmail.com>
2020-01-10 16:25:36 +0200

    proptypes

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,6 @@
 import React , {Component} from 'react';
+import PropTypes from 'prop-types';
+
 import classes from './Person.css';
 import Aux from '../../../hoc/Aux';
 import withClass from '../../../hoc/WithClass';
@@ -18,4 +20,10 @@ class Person extends Component {
   );}
 };
 
+Person.propTypes={
+  click:PropTypes.func ,
+  name:PropTypes.string,
+  age:PropTypes.number,
+  changed:PropTypes.func
+};
 export default withClass(Person,classes.Person);
